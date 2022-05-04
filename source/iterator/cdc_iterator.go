c0f13938f3b2eeb747f0d24bdc7357e7894d6149
hariiwill <hariharan.l@gopherslab.com>
2022-05-04 11:30:11 +0530

    lint-error fix

--- source/iterator/cdc_iterator.go
+++ source/iterator/cdc_iterator.go
@@ -1,3 +1,19 @@
+/*
+Copyright © 2022 Meroxa, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package iterator
 
 import (
@@ -120,7 +136,6 @@ func (c *CDCIterator) Next(ctx context.Context) (sdk.Record, error) {
 
 	c.ticketPosition.AfterURL = res.AfterURL
 	payload, err := json.Marshal(res.TicketList)
-
 	if err != nil {
 		return sdk.Record{}, err
 	}
@@ -128,12 +143,10 @@ func (c *CDCIterator) Next(ctx context.Context) (sdk.Record, error) {
 		AfterURL:     c.ticketPosition.AfterURL,
 		NextIterator: time.Now(),
 	}
-
 	return sdk.Record{
 		Position: ticketIndex.ToRecordPosition(),
 		Payload:  sdk.RawData(payload),
 	}, err
-
 }
 
 func basicAuth(username, apiToken string) string {
